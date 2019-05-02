cbf1a8e3bb3a8772af2897c7932f3f29fedba7d7
simranBhamra <45669456+simranBhamra@users.noreply.github.com>
2019-05-02 14:46:12 -0400

    Update InvMenu.cpp

--- InvMenu.cpp
+++ InvMenu.cpp
@@ -8,6 +8,8 @@ using namespace std;
 //20 books
 int const SIZE = 20; 
 
+
+//this function presents a menu to the user then calls the corresponding function to that selection
 void invMenu()
 {
 	int choice = 0;
@@ -23,6 +25,7 @@ void invMenu()
 		cout << "4. Delete a Book\n";
 		cout << "5. Return to the Main Menu\n\n";
 
+		//taking in user input
 		cout << "Enter Your Choice: ";
 		cin >> choice;
 
@@ -34,6 +37,8 @@ void invMenu()
 			cout << "Enter your choice: ";
 			cin >> choice;
 		}
+		
+		//switch case for choices
 
 		switch (choice)
 		{
@@ -58,28 +63,30 @@ void invMenu()
 	}//end while choice !=5
 }
 
+//function to look up a book 
 void lookUpBook()
 {
 	cout << "\nYou selected Look Up Book.\n";
 }
 
+//adding a book to the array
 void addBook()
 {
 	cout << "\nYou selected add book.\n"; 
 }
-
+//editing the books 
 void editBook()
 {
 	cout << "\nYou selected Edit Book.\n";
 }
-
+//deteling function
 void deleteBook()
 {
 	cout << "\nYou selected Delete Book.\n";
 }
 
 
-
+//search function for the books 
 int findBook( ) {
 	
 	cout << "\nYou selected find book.\n"; 
